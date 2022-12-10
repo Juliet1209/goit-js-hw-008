b25d71e968eecbc0e7e63f70f0d70d8b0ac081e1
Julia <j.spivakovska@gmail.com>
2022-12-10 07:20:41 +0200

    8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,34 +1,41 @@
-import throttle from 'lodash.throttle';
+const throttle = require('lodash.throttle');
 
 const refs = {
- form: document.querySelector(".feedback-form"),
- email: document.querySelector(".feedback-form input"),
- textarea: document.querySelector(".feedback-form textarea"),
- }
- 
-let formData = {};
+    form: document.querySelector(".feedback-form"),
+    input: document.querySelector("input"),
+    textarea: document.querySelector("textarea")
+};
+const formStorage = {};
 const STORAGE_KEY = "feedback-form-state";
+const getFormStorage = localStorage.getItem(STORAGE_KEY);
 
-refs.form.addEventListener("submit", throttle(onFormSubmit, 500));
+refs.form.addEventListener("input", throttle(setValueStorage, 500));
+refs.form.addEventListener("submit", throttle(submitForm, 500))
 
-savedFormData();
+function setValueStorage(event) {
+    formStorage[event.target.name] = event.target.value;
 
-refs.form.addEventListener("input", event => {
-    formData[event.target.name] = event.target.value;
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-    console.log(formData);
-});
+    const valueStorageJSON = JSON.stringify(formStorage);
+    localStorage.setItem(STORAGE_KEY, valueStorageJSON);
+};
 
-function onFormSubmit(event){
+function submitForm(event) {
     event.preventDefault();
+
+    console.log(formStorage);
+
     event.currentTarget.reset();
     localStorage.removeItem(STORAGE_KEY);
 }
-function savedFormData(){
-     const savedFormData = localStorage.getItem(STORAGE_KEY);
-     const parsedFormData = JSON.parse(savedFormData);
-      if (parsedFormData) {
-    formData = parsedFormData;
-    refs.email.value = formData.email || '';
-    refs.textarea.value = formData.message || '';
-    console.log(savedFormData);
+
+
+if (getFormStorage) {
+
+    const getValueStorage = JSON.parse(getFormStorage);
+
+    refs.input.value = getValueStorage.email;
+    refs.textarea.value = getValueStorage.message;
+
+    formStorage.email = getValueStorage.email;
+    formStorage.message = getValueStorage.message;
+}
